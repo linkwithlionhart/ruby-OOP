95c858a260775b781589c2a47aae45ff48e1ca42
linkwithlionhart <linkwithlionhart@gmail.com>
2023-12-04 19:28:05 -0800

    complete ex3

---
+++ classes-and-objects-p1/ex3.rb
@@ -0,0 +1,55 @@
+=begin
+  Add an accessor method to your MyCar class to change and view the color of your car. 
+  Then add an accessor method that allows you to view, but not modify, the year of your car.
+=end
+
+class MyCar
+  attr_accessor :color, :model
+  attr_reader :year
+  # attr_writer :model
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+
+  def current_speed
+    puts "You are moving at #{@current_speed} km/hr."
+  end
+
+  def accelerate(number)
+    @current_speed += number
+    puts "You hit the gas and accelerate by #{number} km/hr!"
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You press the brakes and decelerate by #{number} km/hr."
+  end
+
+  def shutoff
+    @current_speed = 0
+    puts "If slow is steady, then your stop is ready."
+  end
+
+  def spray_paint(new_color)
+    color = new_color
+    puts "Thank you for waiting. Your #{model} is now #{color}. It was previously #{@color}."
+  end
+
+  def what_am_i
+    puts "This car is a #{year}, #{color} #{model}."
+  end
+
+end
+
+endeavor = MyCar.new(2008, 'silver', 'mitsubishi endeavor')
+endeavor.color = 'red'
+endeavor.model = 'lancer'
+puts endeavor.year
+puts endeavor.color
+puts endeavor.model
+endeavor.what_am_i
+endeavor.spray_paint('black')
