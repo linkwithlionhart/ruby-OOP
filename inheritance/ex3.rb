b030eb030b3da7271b655144f49c1c9a3e122a69
linkwithlionhart <linkwithlionhart@gmail.com>
2023-12-04 23:41:52 -0800

    complete ex3

---
+++ inheritance/ex3.rb
@@ -0,0 +1,80 @@
+# Create a module that you can mix in to ONE of your subclasses that describes a behavior unique to that subclass.
+
+module Stowable
+  def can_stow?(bikes)
+    bikes <= 4
+  end
+end
+
+class Vehicle
+  attr_accessor :color
+  attr_reader :year 
+  attr_reader :model
+
+  @@number_of_vehicles = 0
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+    @@number_of_vehicles += 1
+  end
+
+  def self.number_of_vehicles
+    puts "This factory has created #{@@number_of_vehicles} vehicles."
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles/gallons.to_i} miles per gallon of gas."    
+  end
+
+  def current_speed
+    puts "You are moving at #{@current_speed} km/hr."
+  end
+
+  def accelerate(number)
+    @current_speed += number
+    puts "You hit the gas and accelerate by #{number} km/hr!"
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You press the brakes and decelerate by #{number} km/hr."
+  end
+
+  def shutoff
+    @current_speed = 0
+    puts "If slow is steady, then your stop is ready."
+  end
+
+  def spray_paint(new_color)
+    color = new_color
+    puts "Thank you for waiting. Your #{model} is now #{color}. It was previously #{@color}."
+  end
+
+  def what_am_i
+    puts "This car is a #{year}, #{color} #{model}."
+  end
+
+  def to_s
+    "My vehicle is a #{color}, #{year}, #{model}!"
+  end
+
+end
+
+class MyCoupe < Vehicle
+  NUMBER_DOORS = 2
+end
+
+class MySedan < Vehicle
+  include Stowable
+  NUMBER_DOORS = 4
+end
+
+my_coupe = MyCoupe.new('2020', 'Mustang', 'black')
+my_sedan = MySedan.new('2022', 'Honda Civic', 'white')
+
+# puts my_coupe.can_stow?(2) - cannot use because we did not mixin module with class
+puts my_sedan.can_stow?(3)
+puts my_sedan.can_stow?(5)
